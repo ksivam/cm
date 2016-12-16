e99335c89be71a0439b611d284cf6553aea7c2e1
Krishna Sadasivam <chander.krishna@gmail.com>
2016-12-15 22:35:21 -0800

    Add endwith and starts with js files

---
+++ JavaScript/js/endsWith.js
@@ -0,0 +1,33 @@
+﻿(function (String, undefined) {
+    'use strict';
+
+    // NOTE: Tested only in Chrome browser.
+    var endsWith = function (input) {
+        if (input === null ||
+            input === undefined) {
+            // handle null and undefined values.
+            return false;
+        }
+
+        var srcString = String(this);
+        var inputString = String(input);
+
+        if (srcString.length < inputString.length) {
+            return false;
+        }
+
+        var inputStringIndex = inputString.length;
+        var srcStringIndex = srcString.length;
+        while (--inputStringIndex >= 0 && --srcStringIndex) {
+            // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+            if (srcString.charCodeAt(srcStringIndex) !== inputString.charCodeAt(inputStringIndex)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    String.prototype.endsWith = endsWith;
+
+})(String);
---
+++ JavaScript/js/startsWith.js
@@ -0,0 +1,31 @@
+﻿(function (String, undefined) {
+    'use strict';
+
+    // NOTE: Tested only in Chrome browser.
+    var startsWith = function (input) {
+        if (input === null ||
+            input === undefined) {
+            // handle null and undefined values.
+            return false;
+        }
+
+        var srcString = String(this);
+        var inputString = String(input);
+
+        if (srcString.length < inputString.length) {
+            return false;
+        }
+
+        var index = -1;
+        while (++index < inputString.length) {
+            // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+            if (srcString.charCodeAt(index) !== inputString.charCodeAt(index)) {
+                return false;;
+            }
+        }
+
+        return true;
+    };
+
+    String.prototype.startsWith = startsWith;
+})(String);
